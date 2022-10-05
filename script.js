3d5a42163d384f6799dbdaa9f65b4691b436b17b
Aidan <adn.mck@gmail.com>
2022-10-05 01:23:38 +0100

    mobile view added, code finished, needs finishing touches to design

--- script.js
+++ script.js
@@ -3,120 +3,170 @@ const gameBoard = (() => {
     const boardContainer = document.querySelector(".boardContainer");
     const board = [];
 
-    for (let i = 0; i < 9; i++) {
-        let cell = document.createElement("div");
-        cell.id = i;
-        cell.classList.add("boardCell");
-        boardContainer.appendChild(cell);
-        board.push(cell);
+    const buildBoard = (board) => {
+        board = board || [];
+        for (let cellNumber = 0; cellNumber < 9; cellNumber++) {
+            let cell = document.createElement("div");
+            cell.id = cellNumber;
+
+            cell.classList.add("boardCell");
+            boardContainer.appendChild(cell);
+            board.push(cell);
+        }
+        return board;
     }
-    return { board };
+
+    const newBoard = (board) => {
+        board = board || [];
+        for (let index = 0; index < board.length; index++) {
+            board.unshift(index);
+        }
+
+        while (boardContainer.firstChild) {
+            boardContainer.removeChild(boardContainer.lastChild);
+        }
+
+        buildBoard(board);
+        return board;
+    }
+
+    return { newBoard, board, boardContainer };
 
 })();
 
 const Player = (name, token) => {
 
     this.name = name;
+
     this.token = token;
     this.cells = [];
+    this.winner = false;
 
-    return { name, token, cells };
+    console.log(`setting ${this.name}...`)
+
+    return { name, token, cells, winner };
 };
 
 const displayController = (() => {
-    
+   
+    let turn;
+    let currentPlayer;
+
     const playerOne = Player("player one", "X");
     const playerTwo = Player("player two", "O");
-    let turn = 1;
-    let currentPlayer = playerOne;
 
-    for (const cell of gameBoard.board) {
+    const listen = () => {
+        const cells = document.querySelectorAll(".boardCell");        
+        
+        for (const cell of cells) {
             cell.addEventListener("click", takeTurn);
-    }
-
-    function checkWinner(player) {
-
-        // Check for matches from middle cell
-        if (player.cells.includes("4")) {
-            if (player.cells.includes("0")) {
-                if (player.cells.includes("8")) {
-                    return player.name;
-                }
-            } else if (player.cells.includes("1")) {
-                if (player.cells.includes("7")) {
-                    return player.name;
-                }
-            } else if (player.cells.includes("2")) {
-                if (player.cells.includes("6")) {
-                    return player.name;
-                }
-            } else if (player.cells.includes("3")) {
-                if (player.cells.includes("5")) {
-                    return player.name;
-                }
-            }
-        // Check for matches from top left corner
-        } else if (player.cells.includes("0")) {
-            if (player.cells.includes("1")) {
-                if (player.cells.includes("2")) {
-                    return player.name;
-                }
-            } else if (player.cells.includes("3")) {
-                if (player.cells.includes("6")) {
-                    return player.name;
-                }
-            }
-        // Check for matches from bottom right corner
-        } else if (player.cells.includes("8")) {
-            if (player.cells.includes("7")) {
-                if (player.cells.includes("6")) {
-                    return player.name;
-                }
-            } else if (player.cells.includes("5")) {
-                if (player.cells.includes("2")) {
-                    return player.name;
-                }
-            }
         }
     }
 
-    function displayWinner(winner) {
-        
-        const test = document.createElement("div");
-        test.classList.add("popup");
-        
-        if (winner === undefined) {
-            test.textContent = "tie game";
-        } else {   
-            test.textContent = `${winner} wins!`;
+    const removePopup = () => {
+        if (document.body.contains(document.querySelector(".popup"))) {
+            const popup = document.querySelector(".popup");
+            document.body.removeChild(popup);
         }
-        document.body.appendChild(test);
+    }
+    
+    const resetGame = () => {
+        console.log("resetting...");
+        playerOne.winner = false;
+        playerTwo.winner = false;
+        playerOne.cells = [];
+        playerTwo.cells = [];
+        removePopup();
+        setupGame();
+    }
+
+    const setupGame = () => {
+        turn = 1;
+        console.log("setting...");
+        
+        gameBoard.newBoard();
+        listen();
         return;
     }
 
-    function takeTurn(e) {
+    const checkMatches = (cells) => {
+
+        console.log("checking matches...");
+
+        // Check for matches from middle cell
+        if (cells.includes("4")) {
+            if (cells.includes("0") && (cells.includes("8"))) {
+                return true;
+            } else if (cells.includes("1") && (cells.includes("7"))) {
+                return true;
+            } else if (cells.includes("2") && (cells.includes("6"))) {
+                return true;
+            } else if (cells.includes("3") && (cells.includes("5"))) {
+                return true;
+            }
+        // Check for matches from top left corner
+        } else if (cells.includes("0")) {
+            if (cells.includes("1") && (cells.includes("2"))) {
+                return true;
+            } else if (cells.includes("3") && (cells.includes("6"))) {
+                return true;
+            }
+        // Check for matches from bottom right corner
+        } else if (cells.includes("8")) {
+            if (cells.includes("7") && (cells.includes("6"))) {
+                return true;
+            } else if (cells.includes("5") && (cells.includes("2"))) {
+                return true;
+            }
+        }
+    }
+
+    const displayWinner = (currentPlayer) => {
+
+        winner = currentPlayer;
+        const winnerPopup = document.createElement("div");
+        winnerPopup.textContent = "tie game!";
+        winnerPopup.classList.add("popup");
+        
+        if (winner !== undefined) {
+            winnerPopup.textContent = `${winner.name} wins!`;
+        }
+        
+        const resetButton = document.createElement("button");
+        resetButton.textContent = "play again?";
+        resetButton.classList.add("resetButton");
+        winnerPopup.appendChild(resetButton);
+        document.body.appendChild(winnerPopup);
+
+        resetButton.addEventListener("click", resetGame);
+    }
+
+    const takeTurn = (e) => {
+
+        if (playerOne.winner || playerTwo.winner) {
+            return;
+        }
+
+        if (turn === 1) {
+            currentPlayer = playerOne;
+        }
+
         const cell = e.target;
         const id = cell.id;
         
         // Can only play on the selected cell if it's empty
-        if (!cell.textContent) {
+        if (cell.textContent !== "X" && cell.textContent !== "O") {
+            cell.classList.add(currentPlayer.token);
             cell.textContent = gameBoard.board[id] = currentPlayer.token;
             currentPlayer.cells.push(id);
             turn++;
-            if (currentPlayer.cells.length >= 3) {
-                let winner = checkWinner(currentPlayer);
-                console.log(winner);
-                if (winner !== undefined) {
-                    displayWinner(winner);
-                    return;
-                }
-            }
-
-            if (turn === 10) {
-                displayWinner();
+            
+            if (checkMatches(currentPlayer.cells)) {
+                currentPlayer.winner = true;
+                displayWinner(currentPlayer);
                 return;
             }
-
+            
             // Alternate player turns
             if (currentPlayer === playerOne) {
                 currentPlayer = playerTwo;
@@ -124,6 +174,13 @@ const displayController = (() => {
                 currentPlayer = playerOne;
             }
         }
+
+        if (turn === 10) {
+            displayWinner();
+            return;
+        }
     }
+       
+    setupGame();
 
 })();
